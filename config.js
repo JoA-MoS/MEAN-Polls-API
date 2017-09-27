af13f7acab9a76d7c3f5eb946ba6d257b385dfd1
Justin Dietz <justin.r.dietz@gmail.com>
2017-09-27 10:39:02 -0700

    Pre Exam Checkin

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
   environment: process.env.NODE_ENV || 'dev',
   server: {
-    port: process.env.PORT || 8090
+    port: process.env.PORT || 9000
   },
   mongo: {
-    url: process.env.MONGO_DB_URI || 'mongodb://localhost/exam-api'
+    url: process.env.MONGO_DB_URI || 'mongodb://localhost/exam2-api'
   }
 };
 
