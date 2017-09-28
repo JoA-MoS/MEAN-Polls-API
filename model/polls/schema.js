271c2b3bfcf949fc11ba913859445a94c8ffcafc
Justin Dietz <justin.r.dietz@gmail.com>
2017-09-27 17:14:29 -0700

    Min Option and Question text

--- model/polls/schema.js
+++ model/polls/schema.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const optionSchema = new Schema({
-  optionText: { type: String, required: true },
+  optionText: { type: String, required: true, minlength: 3 },
   votes: { type: Number, default: 0 }
 });
 
 const pollSchema = new Schema({
-  questionText: { type: String, required: true },
+  questionText: { type: String, required: true, minlength: 8 },
   options: [optionSchema],
   user: { type: String }
 }, { timestamps: true });
